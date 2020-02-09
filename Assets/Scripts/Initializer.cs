d9b9a94046684f88fe8344ea0d6ec6e660564532
HERO-KUN <herokun.user@gmail.com>
2020-02-09 18:30:22 +0900

    fixed bug that text color not changed when application resumed

--- Assets/Scripts/Initializer.cs
+++ Assets/Scripts/Initializer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,22 +19,21 @@ public class Initializer : MonoBehaviour
     public Text title;
     public Text subtitle;
 
-    public AnimationCurve ac;
-
     GameObject[] digitListHours;
     GameObject[] digitListMinutes;
     GameObject[] digitListSeconds;
-    
-    int movedCount = 0;
 
-    int update = 0;
+    private int update = 0;
+    private bool firstUpdate = true;
 
-    float movementDuration = 0.5f;
+    private const int movementDuration = 65;
+    private readonly AnimationCurve movementCurve = new AnimationCurve();
+
+    private readonly int[][] previousDigitData = { new int[2], new int[2], new int[2] };
 
     // Start is called before the first frame update
     void Start()
     {
-        ac = Interpolators.EaseOutCurve;
 
         digitListHours = new GameObject[] { digitArea.transform.GetChild(0).gameObject, digitArea.transform.GetChild(1).gameObject };
         digitListMinutes = new GameObject[] { digitArea.transform.GetChild(3).gameObject, digitArea.transform.GetChild(4).gameObject };
@@ -51,6 +51,11 @@ public class Initializer : MonoBehaviour
                 digitLists[i][1].transform.GetChild(j).GetComponent<TextMesh>().color = new Color(1f, 1f, 1f, 0.25f);
             }
         }
+
+        Keyframe startFrame = new Keyframe(0, 0) { inTangent = 3, outTangent = 3 };
+        Keyframe lastFrame = new Keyframe(1, 1) { inTangent = 0, outTangent = 0};
+        movementCurve.AddKey(startFrame);
+        movementCurve.AddKey(lastFrame);
 
         if (File.Exists(PREF_PATH)) {
             Preferences prefs = JsonUtility.FromJson<Preferences>(File.ReadAllText(PREF_PATH));
@@ -79,6 +84,9 @@ public class Initializer : MonoBehaviour
             background.color = new Color(0, 0, 0);
             backgroundBright.color = new Color(0, 0, 0);
         }
+
+        update = DateTime.Now.Second;
+
     }
 
     // Update is called once per frame
@@ -90,62 +98,77 @@ public class Initializer : MonoBehaviour
 
         update = currentTime.Second;
 
-        MoveDigits(currentTime.Second, digitListSeconds);
-        MoveDigits(currentTime.Minute, digitListMinutes);
-        MoveDigits(currentTime.Hour, digitListHours);
+        StartCoroutine(UpdateDigits(movementDuration, currentTime.Second, 2, digitListSeconds));
+        StartCoroutine(UpdateDigits(movementDuration, currentTime.Minute, 1, digitListMinutes));
+        StartCoroutine(UpdateDigits(movementDuration, currentTime.Hour, 0, digitListHours));
     }
 
-    private void MoveDigits(int data, GameObject[] digitObjects) {
-        int firstDigit = data / 10;
-        int secondDigit = data - (data / 10 * 10);
-
-        if ((int)(digitObjects[0].transform.localPosition.y) != firstDigit || movedCount <= 6) {
-            AddMovedCount();
-            StartCoroutine(Interpolators.Curve(Interpolators.EaseOutCurve, digitObjects[0].transform.localPosition.y, firstDigit, movementDuration,
-                (step) =>
-                {
-                    digitObjects[0].transform.localPosition = new Vector3(digitObjects[0].transform.localPosition.x, step, 0);
-                }, null));
-
-            StartCoroutine(Interpolators.Curve(Interpolators.LinearCurve, 0.25f, firstDigit == 0 ? 0.5f : 0.8f, 0.25f,
-                (step) =>
-                {
-                    digitObjects[0].transform.GetChild(firstDigit).GetComponent<TextMesh>().color = new Color(1f, 1f, 1f, step);
-                }, null));
-            StartCoroutine(Interpolators.Curve(Interpolators.LinearCurve, firstDigit - 1 == 0 ? 0.5f : 0.8f, 0.25f, 0.25f,
-                (step) =>
-                {
-                    digitObjects[0].transform.GetChild(firstDigit - 1 >= 0 ? firstDigit - 1 : digitObjects[0].transform.childCount - 1).GetComponent<TextMesh>().color = new Color(1f, 1f, 1f, step);
-                }, null));
+    private IEnumerator UpdateDigits(int length, int time, int digitType, GameObject[] digitObjects) {
+        int firstDigitData = time / 10;
+        int secondDigitData = time - (time / 10 * 10);
+        bool updateFirstDigit = previousDigitData[digitType][0] != firstDigitData;
+        bool updateSecondDigit = previousDigitData[digitType][1] != secondDigitData;
+        
+        if ((!updateFirstDigit && !updateSecondDigit) && !firstUpdate) {
+            yield break;
         }
 
-        if ((int)(digitObjects[1].transform.localPosition.y) != secondDigit || movedCount <= 6) {
-            AddMovedCount();
-            StartCoroutine(Interpolators.Curve(Interpolators.EaseOutCurve, digitObjects[1].transform.localPosition.y, secondDigit, movementDuration,
-                (step) =>
-                {
-                    digitObjects[1].transform.localPosition = new Vector3(digitObjects[1].transform.localPosition.x, step, 0);
-                }, null));
+        for (int i = 0; i < length; i++) {
+            if (updateFirstDigit || firstUpdate) {
+                digitObjects[0].transform.localPosition = new Vector3(digitObjects[0].transform.localPosition.x, Mathf.Lerp(previousDigitData[digitType][0], firstDigitData, movementCurve.Evaluate(i/(float)length)), 0);
+                digitObjects[0].transform.GetChild(firstDigitData).GetComponent<TextMesh>().color = new Color(1f, 1f, 1f, i / (float)length * (firstDigitData == 0 ? 0.5f : 0.8f));
+                digitObjects[0].transform.GetChild(previousDigitData[digitType][0]).GetComponent<TextMesh>().color = new Color(1f, 1f, 1f, (previousDigitData[digitType][0] == 0 ? 0.5f : 0.8f) - i / (float)length * (previousDigitData[digitType][0] == 0 ? 0.25f : 0.55f));
+            }
 
-            StartCoroutine(Interpolators.Curve(Interpolators.LinearCurve, 0.25f, 0.8f, 0.25f,
-                (step) =>
-                {
-                    digitObjects[1].transform.GetChild(secondDigit).GetComponent<TextMesh>().color = new Color(1f, 1f, 1f, step);
-                }, null));
-            StartCoroutine(Interpolators.Curve(Interpolators.LinearCurve, 0.8f, 0.25f, 0.25f,
-                (step) =>
-                {
-                    digitObjects[1].transform.GetChild(secondDigit - 1 >= 0 ? secondDigit - 1 : digitObjects[1].transform.childCount - 1).GetComponent<TextMesh>().color = new Color(1f, 1f, 1f, step);
-                }, null));
+            if (updateSecondDigit || firstUpdate) {
+                digitObjects[1].transform.localPosition = new Vector3(digitObjects[1].transform.localPosition.x, Mathf.Lerp(previousDigitData[digitType][1], secondDigitData, movementCurve.Evaluate(i / (float)length)), 0);
+                digitObjects[1].transform.GetChild(secondDigitData).GetComponent<TextMesh>().color = new Color(1f, 1f, 1f, 0.25f + i / (float)length * 0.55f);
+                digitObjects[1].transform.GetChild(previousDigitData[digitType][1]).GetComponent<TextMesh>().color = new Color(1f, 1f, 1f, 0.8f - (i / (float)length * 0.55f));
+            }
+
+            yield return new WaitForEndOfFrame();
         }
 
-    }
-
-    private void AddMovedCount() {
-        movedCount++;
-        if(movedCount > 10) {
-            movedCount = 10;
+        if (updateFirstDigit || firstUpdate) {
+            digitObjects[0].transform.localPosition = new Vector3(digitObjects[0].transform.localPosition.x, firstDigitData, 0);
+            previousDigitData[digitType][0] = firstDigitData;
         }
+        if (updateSecondDigit || firstUpdate) {
+            digitObjects[1].transform.localPosition = new Vector3(digitObjects[1].transform.localPosition.x, secondDigitData, 0);
+            previousDigitData[digitType][1] = secondDigitData;
+        }
+
+        if (digitType == 0) {
+            for (int i = 0; i < digitListHours[0].transform.childCount; i++) {
+                if (i != firstDigitData)
+                    digitListHours[0].transform.GetChild(i).GetComponent<TextMesh>().color = new Color(1, 1, 1, 0.25f);
+            }
+            for (int i = 0; i < digitListHours[1].transform.childCount; i++) {
+                if (i != secondDigitData)
+                    digitListHours[1].transform.GetChild(i).GetComponent<TextMesh>().color = new Color(1, 1, 1, 0.25f);
+            }
+        } else if (digitType == 1) {
+            for (int i = 0; i < digitListMinutes[0].transform.childCount; i++) {
+                if (i != firstDigitData)
+                    digitListMinutes[0].transform.GetChild(i).GetComponent<TextMesh>().color = new Color(1, 1, 1, 0.25f);
+            }
+            for (int i = 0; i < digitListMinutes[1].transform.childCount; i++) {
+                if (i != secondDigitData)
+                    digitListMinutes[1].transform.GetChild(i).GetComponent<TextMesh>().color = new Color(1, 1, 1, 0.25f);
+            }
+        } else {
+            for (int i = 0; i < digitListSeconds[0].transform.childCount; i++) {
+                if (i != firstDigitData)
+                    digitListSeconds[0].transform.GetChild(i).GetComponent<TextMesh>().color = new Color(1, 1, 1, 0.25f);
+            }
+            for (int i = 0; i < digitListSeconds[1].transform.childCount; i++) {
+                if (i != secondDigitData)
+                    digitListSeconds[1].transform.GetChild(i).GetComponent<TextMesh>().color = new Color(1, 1, 1, 0.25f);
+            }
+        }
+
+        firstUpdate = false;
+
     }
 
 }
